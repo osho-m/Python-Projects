cfa4b9e2ebc9ec866a3e62c46367a56215426525
Osho M <oshomuralidaran@gmail.com>
2022-09-17 21:08:17 +0530

    Create EmailClasss.py

---
+++ EmailClasss.py
@@ -0,0 +1,17 @@
+class Email:
+      def __init__(self):
+            self.is_sent = False
+
+      def send_email(self):
+            self.is_sent = True
+            
+def __init__(self, name, age):
+      self.name = name
+      self.name = name
+      Dog.dogCount += 1
+
+
+hasattr(emp1, 'age')    # Returns true if 'age' attribute exists
+getattr(emp1, 'age')    # Returns value of 'age' attribute
+setattr(emp1, 'age', 8) # Set attribute 'age' at 8
+delattr(empl, 'age')    # Delete attribute 'age'
