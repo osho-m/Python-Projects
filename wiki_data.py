ada38458f8fd78687f17b8ea81582a3d5fe6464f
Osho M <oshomuralidaran@gmail.com>
2022-09-20 20:45:04 +0530

    Create wiki_data.py

---
+++ wiki_data.py
@@ -0,0 +1,10 @@
+import wikipedia
+print(wikipedia.summary('wikipedia'));
+wikipedia.search('branch');
+ny=wikipedia.page('New Yourk');
+ny.title;
+ny.url;
+print(ny.title, ny.url)
+##ny.contentny.links[0];
+##wikipedia.set.long["for"];
+##wikipedia.summaru("facebook", sentences=1);
