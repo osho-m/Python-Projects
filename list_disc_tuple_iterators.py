e51b9a54fe2c5d1e9afe1910ab31abbf6933a00a
Osho M <oshomuralidaran@gmail.com>
2022-10-02 10:57:40 +0530

    Create list_disc_tuple_iterators.py

---
+++ list_disc_tuple_iterators.py
@@ -0,0 +1,12 @@
+l=["cow", "cat", "Dog"];
+for i in l:
+      print(i)
+t=("cow", "cat", "Dog");
+for i in t:
+      print(i)
+set=(["cow", "cat", "Dog"]);
+for i in set:
+      print(i)
+D={"m1":"cow","m2": "cat","m3": "Dog"};
+for i in D.iterkeys():
+      print(i)
