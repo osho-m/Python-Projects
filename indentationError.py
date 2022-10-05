27144e07eecaccb50e0b6e77156d5c2dc780e3bb
Osho M <oshomuralidaran@gmail.com>
2022-10-05 10:45:07 +0530

    Create indentationError.py

---
+++ indentationError.py
@@ -0,0 +1,14 @@
+##n1=int(input("enter n1 value"))
+##n2=int(input("enter n2 value"))
+##if n1>n2:
+##print("n1 is big")
+##else:
+##print("n2 is big")
+
+
+n1=int(input("enter n1 value"))
+n2=int(input("enter n2 value"))
+if n1>n2:
+      print("n1 is big")
+else:
+      print("n2 is big")
