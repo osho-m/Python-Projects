97a2bdfefaac280d2430ca0b2121eacb579e8366
Osho M <oshomuralidaran@gmail.com>
2022-10-29 21:58:45 +0530

    Create Pythagorian.py

---
+++ Pythagorian.py
@@ -0,0 +1,11 @@
+import math;
+
+x1=int(input("enter x1 value: "))
+y1=int(input("enter y1 value: "))
+x2=int(input("enter x2 value: "))
+y2=int(input("enter y2 value: "))
+
+
+d=math.sqrt((x1-x2)**2+(y1-y2)**2);
+
+print(d);
